f57d2c504a0fce10665dde5e4d43eac24d1ae0c6
sforer580 <sforer580@gmail.com>
2017-01-25 14:41:10 -0800

    Initial Push

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  SF_HW_0
+//
+//  Created by Scott S Forer on 1/25/17.
+//  Copyright © 2017 Scott S Forer. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace  std;
+
+int main() {
+    // insert code here...
+    cout << "Hello, World!" << endl;
+    return 0;
+}
